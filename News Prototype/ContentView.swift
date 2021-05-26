172a9edf0ae3a9393cae0613bc0e0973f2f9f8ab
Matthew Paul O'Brien <matpobrien@gmail.com>
2021-05-26 15:28:54 +0200

    Initial Commit

---
+++ News Prototype/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News Prototype
+//
+//  Created by Mat on 26.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ News Prototype/News_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_PrototypeApp.swift
+//  News Prototype
+//
+//  Created by Mat on 26.05.21.
+//
+
+import SwiftUI
+
+@main
+struct News_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
